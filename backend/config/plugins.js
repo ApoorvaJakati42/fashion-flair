f29ec1cdbb3b1f749bbe3086ef2152ee014cf6f9
Aishwarya-Jakati <jakati.aishwaryaa97@gmail.com>
2023-06-02 13:16:28 +0530

    Installed aws-s3 upload provider and updated the config

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -8,5 +8,18 @@ module.exports = ({env}) => ({
             defaultFrom : "apoorva.jakati.react@gmail.com",
             defaultTo : "apoorva.jakati.react@gmail.com"
         }
-    }}
+    },
+    upload : {
+        provider : "aws-s3" ,
+        providerOptions : {
+            accessKeyId : env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET') ,
+            region: env('AWS_REGION') ,
+            params : {
+                Bucket : env('AWS_BUCKET_NAME')
+            }
+        }
+    }
+
+}
 )
